f3cb3a70b95e7c65a055c2b52cb10909cc4d8d81
Kax-codeslayer <elvinkako17@gmail.com>
2020-08-29 00:04:56 +0200

    commit2 component structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header><a href="#">CapAwes</a></header>
-      <main>productlist</main>
-      <footer>All rights reserved</footer>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort:""
+    }
+  }
+
+  render(){
+    return (
+      <div className="grid-container">
+        <header><a href="#">CapAwes</a></header>
+        <main>
+          <div className="content">
+            <div className="main">products</div>          
+            <div className="sidebar">
+              Cart
+            </div>
+          </div>
+        </main>
+        <footer>All rights reserved</footer>
+      </div>
+    );
+  }
+
 }
 
 export default App;
