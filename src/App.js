fa253dcdc14fe9f5d91695024e1335fc40b44425
Kax-codeslayer <elvinkako17@gmail.com>
2020-08-28 22:58:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header><a href="#">CapAwes</a></header>
+      <main>productlist</main>
+      <footer>All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
